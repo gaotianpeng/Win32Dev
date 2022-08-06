1cdd169c15c35bdbb7ba38100cd2efda55dd1665
gaotianpeng <gtpsydm@gmail.com>
2022-08-06 16:26:44 +0800

    daily practise

--- Win32Dev/win_main.cc
+++ Win32Dev/win_main.cc
@@ -6,6 +6,8 @@
 #include "gapp.h"
 using namespace std;
 
+#pragma comment(lib,"ComCtl32.lib") 
+
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR lpCmdLine, int cmd_show) {
 	GApp app;
 	return app.Run();
