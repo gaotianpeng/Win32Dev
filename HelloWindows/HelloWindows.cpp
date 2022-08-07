e0dc68fb60bcf3983c05b5a11e7fbdea9289cd28
gaotianpeng <gtpsydm@gmail.com>
2022-08-08 00:03:25 +0800

    daily practise

--- HelloWindows/HelloWindows.cpp
+++ HelloWindows/HelloWindows.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <tchar.h>                  // _tcslen函数需要该头文件
 #include "HelloDLL.h"
+#include "resource.h"
 
 #pragma comment(lib, "Winmm.lib")   // 播放声音的PlaySound函数需要Winmm导入库
 #pragma comment(lib, "HelloDLL.lib")
@@ -10,8 +11,7 @@
 // 函数声明，窗口过程
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     WNDCLASSEX wndclass;                        // RegisterClassEx函数用的WNDCLASSEX结构
     TCHAR szClassName[] = TEXT("MyWindow");     // RegisterClassEx函数注册的窗口类的名称
     TCHAR szAppName[] = TEXT("HelloWindows");   // 窗口标题
@@ -27,34 +27,43 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
     wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
     wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-    wndclass.lpszMenuName = NULL;
+    //wndclass.lpszMenuName = NULL;
+    wndclass.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);  // 加载菜单资源 方法1
     wndclass.lpszClassName = szClassName;
     wndclass.hIconSm = NULL;
     RegisterClassEx(&wndclass);
 
+    // 加载菜单资源 方法2
+    //HMENU hMenu;
+    //hMenu = LoadMenu(hInstance, MAKEINTRESOURCE(IDR_MENU1));
+    //hwnd = CreateWindowEx(0, szClassName, szAppName, WS_OVERLAPPEDWINDOW,
+    //    CW_USEDEFAULT, CW_USEDEFAULT, 300, 180, NULL, hMenu, hInstance, NULL);
+
     hwnd = CreateWindowEx(0, szClassName, szAppName, WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 300, 180, NULL, NULL, hInstance, NULL);
+        CW_USEDEFAULT, CW_USEDEFAULT, 500, 500, NULL, NULL, hInstance, NULL);
 
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
 
-    while (GetMessage(&msg, NULL, 0, 0) != 0)
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+    HACCEL hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR1));
+    while (GetMessage(&msg, NULL, 0, 0) != 0) {
+        if (!TranslateAccelerator(hwnd, hAccel, &msg)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+        }
     }
 
     return msg.wParam;
 }
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     HDC hdc;
     PAINTSTRUCT ps1;
     TCHAR szStr[] = TEXT("你好，Windows程序设计");
+    HMENU hMenu;
+    TCHAR szBuf[64] = { 0 };
 
-    switch (uMsg)
-    {
+    switch (uMsg) {
     case WM_CREATE: {
 		PlaySound(TEXT("成都(两会版).wav"), NULL, SND_FILENAME | SND_ASYNC/* | SND_LOOP*/);
 		printf("%d\n", funcAdd(1, 2));
@@ -62,6 +71,11 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         ps.x = 10;
         ps.y = 10;
         printf("%d %d %d\n", nValue, ps.x, ps.y);
+
+        // 加载菜单资源 方法3
+        //hMenu = LoadMenu(g_hInstace, MAKEINTRESOURCE(IDR_MENU1));
+        //SetMenu(hwnd, hMenu);
+
 		return 0;
     }
     case WM_PAINT:
@@ -69,15 +83,45 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         TextOut(hdc, 10, 10, szStr, _tcslen(szStr));
         EndPaint(hwnd, &ps1);
         return 0;
-
+    case WM_SIZE:
+        InvalidateRect(hwnd, NULL, true);
+        return 0;
     case WM_DESTROY:
         PostQuitMessage(0);
         return 0;
     case WM_ACTIVATE:
         printf("window is activated \n");
-        break;
+        return 0;
     case WM_KEYDOWN:
-        break;
+        return 0;
+
+	case WM_COMMAND:
+		printf("WM_COMMAND %d\n", LOWORD(wParam));
+		switch (LOWORD(wParam)) {
+		case ID_FILE_NEW:
+			wsprintf(szBuf, TEXT("您单击了 新建 菜单项 命令ID %d\n"), ID_FILE_NEW);
+			MessageBox(hwnd, szBuf, TEXT("提示"), MB_OK);
+			break;
+		case ID_EDIT_CUT:
+			wsprintf(szBuf, TEXT("您单击了 前切 菜单项 命令ID %d\n"), ID_FILE_NEW);
+			MessageBox(hwnd, szBuf, TEXT("提示"), MB_OK);
+			break;
+		}
+
+		return 0;
+	case WM_SYSCOMMAND:
+		printf("WM_SYSCOMMAND %d\n", LOWORD(wParam));
+		switch (LOWORD(wParam) & 0xFFF0) {
+		case SC_CLOSE:
+			wsprintf(szBuf, TEXT("您单击了 系统菜单 关闭 菜单项 \n"));
+			MessageBox(hwnd, szBuf, TEXT("提示"), MB_OK);
+            SendMessage(hwnd, WM_CLOSE, 0, 0);
+			break;
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+
+		return 0;
     }
 
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
--- HelloWindows/resource.h
+++ HelloWindows/resource.h
@@ -3,19 +3,24 @@
 // π© HelloWindows.rc  π”√
 //
 #define IDR_MENU1                       101
-#define ID_40001                        40001
-#define ID_40002                        40002
-#define ID_40003                        40003
-#define ID_40004                        40004
-#define ID_40005                        40005
-#define ID_40006                        40006
+#define IDR_ACCELERATOR1                102
+#define ID_FILE_NEW                     40007
+#define ID_FILE_OPEN                    40008
+#define ID_FILE_SAVE                    40009
+#define ID_FILE_SAVEAS                  40010
+#define ID_FILE_EXIT                    40011
+#define ID_EDIT_CUT                     40015
+#define ID_EDIT_COPY                    40016
+#define ID_EDIT_PASTE                   40017
+#define ID_HELP_ABOUT                   40018
+#define ID_ACCELERATOR40022             40022
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40007
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40024
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
